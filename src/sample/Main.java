0c22c70226d882fa64aeb84dcddaa1e5a92a4ec3
Michael Skrzypietz <michael.s97@web.de>
2018-02-16 16:51:52 +0100

    changed app title and app size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Cell;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Cell[][] cell =  new Cell[64][64];
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Langton's Ant");
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
