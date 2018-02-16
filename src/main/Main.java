8b0c107cc212f9c515f221343fba7f8c1dcf6ffb
Michael Skrzypietz <michael.s97@web.de>
2018-02-16 21:01:40 +0100

    set app to not resizable and adjusted scene size

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,21 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Cell;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private Cell[][] cell =  new Cell[64][64];
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Langton's Ant");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 790, 835));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
