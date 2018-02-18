09f68db29d6d19cf3d2cb82643773a845ad0cfce
Michael Skrzypietz <michael.s97@web.de>
2018-02-18 19:11:35 +0100

    added grid class

---
+++ src/base/Grid.java
@@ -0,0 +1,17 @@
+package base;
+
+import javafx.scene.layout.GridPane;
+
+public class Grid {
+
+    private GridPane grid;
+
+
+
+    public Grid(GridPane grid) {
+        this.grid = grid;
+    }
+
+
+
+}
